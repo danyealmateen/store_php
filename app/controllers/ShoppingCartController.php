27d103759fed6b686c9e1fa5459177ecd4b73eb5
Danyeal Mateen <danyeal.mateen@hotmail.com>
2024-02-03 18:55:38 +0100

    path to ShowProducts.php included in index.php

--- app/controllers/ShoppingCartController.php
+++ app/controllers/ShoppingCartController.php
@@ -1,5 +1,6 @@
 <?php
-require_once '../models/Products.php';
+
+require "/xampp/htdocs/store/app/models/Products.php";
 
 $product1 = new Products("Produkt A", 10.99, 50);
 $product2 = new Products("Produkt B", 19.99, 30);
