1b45d2192401afd1efaec9c7ac640601f0cb825e
Danyeal Mateen <danyeal.mateen@hotmail.com>
2024-02-03 18:31:31 +0100

    added product class

--- app/models/Products.php
+++ app/models/Products.php
@@ -1,5 +1,26 @@
-<?php 
+<?php
+class Products
+{
+    private $name;
+    private $price;
+    private $stock;
 
-class Products {
-    
+    public function __construct($name, $price, $stock)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->stock = $stock;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function getStock()
+    {
+        return $this->stock;
+    }
 }
