c52140a547ddf4059beb2b792e7237b8e469a0ac
Danyeal Mateen <danyeal.mateen@hotmail.com>
2024-02-07 12:08:48 +0100

    product controller added

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,12 +1,20 @@
 <?php
 
+namespace App\Controllers;
+
 use App\models\ProductModel;
 
 class ProductController
 {
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new ProductModel();
+    }
     public function showProducts()
     {
-        $productModel = new ProductModel();
-        $products = $productModel->getAllProducts();
+        // $productModel = new ProductModel();
+        $products = $this->model->getAllProducts();
     }
 }
