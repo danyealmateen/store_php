10aed3d4a0011bd2900e0295a358460cd21ff9ec
Danyeal Mateen <danyeal.mateen@hotmail.com>
2024-02-08 15:11:52 +0100

    addToCart function implemented

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -9,6 +9,18 @@ class CartController
     public function addToCart($productId, $quantity)
     {
         $productModel = new Product();
-        $productModel->decreaseStock($productId, $quantity);
+
+        try {
+            $productModel->decreaseStock($productId, $quantity);
+
+            //Startar en session för att tilldena användaren ett unikt id, spårar användarens data och interkationer under sessionens livslängd
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+
+            //Logik för att lägga till produkten i kundkorgen.
+        } catch (\Exception $e) {
+            //Hantera fel
+        }
     }
 }
